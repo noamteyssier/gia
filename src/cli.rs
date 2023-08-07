c9614b6837913d796a285f3559980526f23ac137
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-08-07 13:49:40 -0700

    reorder commands

--- src/cli.rs
+++ src/cli.rs
@@ -8,29 +8,6 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Command {
-    /// Extracts FASTA sequences using intervals from a BED file
-    GetFasta {
-        /// BED file containing intervals to extract
-        #[clap(short, long)]
-        bed: Option<String>,
-
-        /// FASTA file to extract sequences from (assumes <fasta>.fai exists)
-        #[clap(short, long)]
-        fasta: String,
-
-        /// Output FASTA file to write to (default=stdout)
-        #[clap(short, long)]
-        output: Option<String>,
-
-        /// Name map file to use (in case chromosome names are non-integers)
-        #[clap(short, long)]
-        map: Option<String>,
-
-        /// Number of threads to use (use zero for all available cores)
-        #[clap(short, long, default_value = "1")]
-        threads: Option<usize>,
-    },
-
     /// Intersects two BED files
     Intersect {
         #[clap(short, long)]
@@ -58,6 +35,40 @@ pub enum Command {
         sorted: bool,
     },
 
+    /// Sorts a BED file by chromosome, start, and end
+    Sort {
+        /// Input GIA file to sort (default=stdin)
+        #[clap(short, long)]
+        input: Option<String>,
+
+        /// Output GIA file to write to (default=stdout)
+        #[clap(short, long)]
+        output: Option<String>,
+    },
+
+    /// Extracts FASTA sequences using intervals from a BED file
+    GetFasta {
+        /// BED file containing intervals to extract
+        #[clap(short, long)]
+        bed: Option<String>,
+
+        /// FASTA file to extract sequences from (assumes <fasta>.fai exists)
+        #[clap(short, long)]
+        fasta: String,
+
+        /// Output FASTA file to write to (default=stdout)
+        #[clap(short, long)]
+        output: Option<String>,
+
+        /// Name map file to use (in case chromosome names are non-integers)
+        #[clap(short, long)]
+        map: Option<String>,
+
+        /// Number of threads to use (use zero for all available cores)
+        #[clap(short, long, default_value = "1")]
+        threads: Option<usize>,
+    },
+
     /// Builds a two column map of chromosome names to integers
     /// and writes the map and BED file with integer chromosome names
     /// to disk
@@ -77,16 +88,5 @@ pub enum Command {
         /// Output map file to write to (default=name_map.tsv)
         #[clap(short, long)]
         map: Option<String>,
-    },
-
-    /// Sorts a BED file by chromosome, start, and end
-    Sort {
-        /// Input GIA file to sort (default=stdin)
-        #[clap(short, long)]
-        input: Option<String>,
-
-        /// Output GIA file to write to (default=stdout)
-        #[clap(short, long)]
-        output: Option<String>,
     },
 }
