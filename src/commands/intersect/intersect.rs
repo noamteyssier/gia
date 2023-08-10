f7b78d249d6d0cb586413935dffb221a6324b199
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-08-09 17:28:01 -0700

    whtiespace

--- src/commands/intersect/intersect.rs
+++ src/commands/intersect/intersect.rs
@@ -1,8 +1,6 @@
 use anyhow::Result;
 use bedrs::{Container, GenomicIntervalSet};
-
 use crate::io::{match_input, match_output, read_set, write_records_iter};
-
 use super::{
     find::{run_find, OverlapMethod},
     iter::{run_function, OutputMethod},
