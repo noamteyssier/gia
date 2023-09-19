dd1bf986dcab1393bc83dbdeaad51d543daab09c
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-09-18 18:42:47 -0700

    added a reordering step to the sorting process for natural language sorting

--- src/commands/sort.rs
+++ src/commands/sort.rs
@@ -1,10 +1,24 @@
-use crate::io::{match_input, match_output, read_set_with, write_set_with};
+use crate::{io::{match_input, match_output, read_set_with, write_set_with}, types::{Translater, Retranslater}};
 use anyhow::Result;
-use bedrs::Container;
+use bedrs::{Container, Coordinates, GenomicIntervalSet};
+
+pub fn reorder_chroms(set: &mut GenomicIntervalSet<usize>, translater: Translater) -> Retranslater {
+    let retranslate = translater.lex_sort();
+    set.apply_mut(|iv| {
+        let new_chr = retranslate.get_rank(*iv.chr()).unwrap();
+        iv.update_chr(&new_chr);
+    });
+    retranslate
+}
 
 pub fn sort(input: Option<String>, output: Option<String>, named: bool) -> Result<()> {
     let input_handle = match_input(input)?;
     let (mut set, translater) = read_set_with(input_handle, named)?;
+    let translater = if let Some(t) = translater {
+        Some(reorder_chroms(&mut set, t))
+    } else {
+        None
+    };
     set.sort();
     let output_handle = match_output(output)?;
     write_set_with(&set, output_handle, translater.as_ref())?;
