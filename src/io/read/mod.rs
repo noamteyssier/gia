d456ab0adf15d780ab72361980fa2c25d4cc0948
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-09-19 16:23:07 -0700

    added bed6 read methods that mirror the bed3 ones

--- src/io/read/mod.rs
+++ src/io/read/mod.rs
@@ -7,7 +7,11 @@ pub mod genome;
 pub mod utils;
 pub use bed3::{
     iter_bed3_unnamed, read_bed3_set, read_bed3_set_named, read_bed3_set_unnamed,
-    read_paired_bed3_named,
+    read_paired_bed3_named, read_paired_bed3_sets,
+};
+pub use bed6::{
+    read_bed6_set, read_bed6_set_named, read_bed6_set_unnamed, read_paired_bed6_named,
+    read_paired_bed6_sets,
 };
 pub use genome::read_genome;
 pub use utils::{build_reader, read_format_set_with};
