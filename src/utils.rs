d2fb956a8bb2282d37740ae5d6e1443a9360b47b
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-09-19 16:23:40 -0700

    added paired sort convenience method to utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
+use bedrs::{
+    traits::{ChromBounds, IntervalBounds, ValueBounds},
+    Container,
+};
 use rand::{thread_rng, RngCore, SeedableRng};
 use rand_chacha::ChaChaRng;
 
@@ -5,5 +9,24 @@ pub fn build_rng(seed: Option<usize>) -> Box<dyn RngCore> {
     match seed {
         Some(seed) => Box::new(ChaChaRng::seed_from_u64(seed as u64)),
         None => Box::new(thread_rng()),
+    }
+}
+
+/// Sorts two sets if they are not already sorted
+pub fn sort_pairs<C, T, I>(
+    set_a: &mut impl Container<C, T, I>,
+    set_b: &mut impl Container<C, T, I>,
+    sorted: bool,
+) where
+    C: ChromBounds,
+    T: ValueBounds,
+    I: IntervalBounds<C, T>,
+{
+    if !sorted {
+        set_a.sort();
+        set_b.sort();
+    } else {
+        set_a.set_sorted();
+        set_b.set_sorted();
     }
 }
