069f6e66c4ec69ec36cc5d0cdb7b44cae9e39634
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-09-20 09:10:12 -0700

    test: included tests of bed6 file format in get fasta.

--- tests/get_fasta.rs
+++ tests/get_fasta.rs
@@ -5,7 +5,7 @@ mod testing {
     use std::process::Command;
 
     #[test]
-    fn test_get_fasta() -> Result<()> {
+    fn test_get_fasta_bed3() -> Result<()> {
         let input = "tests/datasets/get_fasta/unnamed.bed";
         let fasta = "tests/datasets/get_fasta/unnamed.fa";
         let mut cmd = Command::cargo_bin("gia")?;
@@ -22,7 +22,7 @@ mod testing {
     }
 
     #[test]
-    fn test_get_fasta_named() -> Result<()> {
+    fn test_get_fasta_named_bed3() -> Result<()> {
         let input = "tests/datasets/get_fasta/named.bed";
         let fasta = "tests/datasets/get_fasta/named.fa";
         let mut cmd = Command::cargo_bin("gia")?;
@@ -34,6 +34,44 @@ mod testing {
             .arg(fasta)
             .output()?;
         let expected = ">chr1:20-30\nAGCGACTACG\n>chr2:30-40\nCGATCGATCG\n";
+        assert_eq!(String::from_utf8_lossy(&output.stdout), expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_fasta_bed6() -> Result<()> {
+        let input = "tests/datasets/get_fasta/unnamed.bed6";
+        let fasta = "tests/datasets/get_fasta/unnamed.fa";
+        let mut cmd = Command::cargo_bin("gia")?;
+        let output = cmd
+            .arg("get-fasta")
+            .arg("-b")
+            .arg(input)
+            .arg("-f")
+            .arg(fasta)
+            .arg("-F")
+            .arg("bed6")
+            .output()?;
+        let expected = ">1:20-30::0::0::+\nAGCGACTACG\n>2:30-40::0::0::-\nCGATCGATCG\n";
+        assert_eq!(String::from_utf8_lossy(&output.stdout), expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_fasta_named_bed6() -> Result<()> {
+        let input = "tests/datasets/get_fasta/named.bed6";
+        let fasta = "tests/datasets/get_fasta/named.fa";
+        let mut cmd = Command::cargo_bin("gia")?;
+        let output = cmd
+            .arg("get-fasta")
+            .arg("-b")
+            .arg(input)
+            .arg("-f")
+            .arg(fasta)
+            .arg("-F")
+            .arg("bed6")
+            .output()?;
+        let expected = ">chr1:20-30::0::0::+\nAGCGACTACG\n>chr2:30-40::0::0::-\nCGATCGATCG\n";
         assert_eq!(String::from_utf8_lossy(&output.stdout), expected);
         Ok(())
     }
