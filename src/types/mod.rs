858d06ae6908fef4b4502bd7d1f83fecdfac5b1a
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-09-21 09:59:16 -0700

    feat: add a reorder trait which operates on different coordinate types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -3,4 +3,4 @@ mod pairs;
 mod translater;
 pub use formats::{Bed6, FieldFormat, InputFormat, NumericBed6, NumericBed6Set};
 pub use pairs::IntervalPair;
-pub use translater::{Retranslater, StreamTranslater, Translate, Translater};
+pub use translater::{Reorder, Retranslater, StreamTranslater, Translate, Translater};
