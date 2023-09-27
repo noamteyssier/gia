1f1331bf813e757e38ce1f678a169484be6c26db
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-09-27 15:15:44 -0700

    tests: added testing for gzip and bgzip IO

--- src/io/general.rs
+++ src/io/general.rs
@@ -65,3 +65,135 @@ pub fn match_output(
         }
     }
 }
+
+#[cfg(test)]
+mod testing {
+
+    use std::fs::remove_file;
+
+    use super::*;
+
+    const TEST_BED3: &str = "tests/datasets/io/bed3.bed";
+    const TEST_BED3_GZ: &str = "tests/datasets/io/bed3.bed.gz";
+    const TEST_BED3_BGZ: &str = "tests/datasets/io/bed3.bed.bgz";
+
+    const TEST_BED6: &str = "tests/datasets/io/bed6.bed";
+    const TEST_BED6_GZ: &str = "tests/datasets/io/bed6.bed.gz";
+    const TEST_BED6_BGZ: &str = "tests/datasets/io/bed6.bed.bgz";
+
+    const EXP_SIZE: usize = 10;
+
+    #[test]
+    fn test_read_bed3_plaintext() {
+        let filename = TEST_BED3.to_string();
+        let handle = match_input(Some(filename)).unwrap();
+        let lines = handle.lines().count();
+        assert_eq!(lines, EXP_SIZE);
+    }
+
+    #[test]
+    fn test_read_bed3_gz() {
+        let filename = TEST_BED3_GZ.to_string();
+        let handle = match_input(Some(filename)).unwrap();
+        let lines = handle.lines().count();
+        assert_eq!(lines, EXP_SIZE);
+    }
+
+    #[test]
+    fn test_read_bed3_bgz() {
+        let filename = TEST_BED3_BGZ.to_string();
+        let handle = match_input(Some(filename)).unwrap();
+        let lines = handle.lines().count();
+        assert_eq!(lines, EXP_SIZE);
+    }
+
+    #[test]
+    fn test_read_bed6_plaintext() {
+        let filename = TEST_BED6.to_string();
+        let handle = match_input(Some(filename)).unwrap();
+        let lines = handle.lines().count();
+        assert_eq!(lines, EXP_SIZE);
+    }
+
+    #[test]
+    fn test_read_bed6_gz() {
+        let filename = TEST_BED6_GZ.to_string();
+        let handle = match_input(Some(filename)).unwrap();
+        let lines = handle.lines().count();
+        assert_eq!(lines, EXP_SIZE);
+    }
+
+    #[test]
+    fn test_read_bed6_bgz() {
+        let filename = TEST_BED6_BGZ.to_string();
+        let handle = match_input(Some(filename)).unwrap();
+        let lines = handle.lines().count();
+        assert_eq!(lines, EXP_SIZE);
+    }
+
+    #[test]
+    fn test_write_bed3_plaintext() {
+        let filename = "null.bed3".to_string();
+        let handle = match_output(Some(filename), 1, 1).unwrap();
+        let mut writer = BufWriter::new(handle);
+        for _ in 0..EXP_SIZE {
+            writer.write_all(b"chr1\t1\t2\n").unwrap();
+        }
+        remove_file("null.bed3").unwrap();
+    }
+
+    #[test]
+    fn test_write_bed3_gz() {
+        let filename = "null.bed3.gz".to_string();
+        let handle = match_output(Some(filename), 1, 1).unwrap();
+        let mut writer = BufWriter::new(handle);
+        for _ in 0..EXP_SIZE {
+            writer.write_all(b"chr1\t1\t2\n").unwrap();
+        }
+        remove_file("null.bed3.gz").unwrap();
+    }
+
+    #[test]
+    fn test_write_bed3_bgz() {
+        let filename = "null.bed3.bgz".to_string();
+        let handle = match_output(Some(filename), 1, 1).unwrap();
+        let mut writer = BufWriter::new(handle);
+        for _ in 0..EXP_SIZE {
+            writer.write_all(b"chr1\t1\t2\n").unwrap();
+        }
+        remove_file("null.bed3.bgz").unwrap();
+    }
+
+    #[test]
+    fn test_write_bed6_plaintext() {
+        let filename = "null.bed6".to_string();
+        let handle = match_output(Some(filename), 1, 1).unwrap();
+        let mut writer = BufWriter::new(handle);
+        for _ in 0..EXP_SIZE {
+            writer.write_all(b"chr1\t1\t2\tname\t0\t+\n").unwrap();
+        }
+        remove_file("null.bed6").unwrap();
+    }
+
+    #[test]
+    fn test_write_bed6_gz() {
+        let filename = "null.bed6.gz".to_string();
+        let handle = match_output(Some(filename), 1, 1).unwrap();
+        let mut writer = BufWriter::new(handle);
+        for _ in 0..EXP_SIZE {
+            writer.write_all(b"chr1\t1\t2\tname\t0\t+\n").unwrap();
+        }
+        remove_file("null.bed6.gz").unwrap();
+    }
+
+    #[test]
+    fn test_write_bed6_bgz() {
+        let filename = "null.bed6.bgz".to_string();
+        let handle = match_output(Some(filename), 1, 1).unwrap();
+        let mut writer = BufWriter::new(handle);
+        for _ in 0..EXP_SIZE {
+            writer.write_all(b"chr1\t1\t2\tname\t0\t+\n").unwrap();
+        }
+        remove_file("null.bed6.bgz").unwrap();
+    }
+}
