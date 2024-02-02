b93546d56f1b99acbc06f4b6085bab310571f6af
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-02-02 10:58:53 -0800

    tests: added testing functions for flank

--- src/commands/flank.rs
+++ src/commands/flank.rs
@@ -31,8 +31,10 @@ where
         Box::new([left.unwrap(), right.unwrap()].into_iter())
     } else if left.is_some() {
         Box::new(std::iter::once(left.unwrap()))
-    } else {
+    } else if right.is_some() {
         Box::new(std::iter::once(right.unwrap()))
+    } else {
+        Box::new(std::iter::empty())
     }
 }
 
@@ -169,4 +171,154 @@ pub fn flank(
     let bed = BedReader::from_path(input, input_format, field_format)?;
     let output_handle = match_output(output, compression_threads, compression_level)?;
     dispatch_flank(bed, genome_path, both, left, right, output_handle)
+}
+
+#[cfg(test)]
+mod testing {
+
+    use super::*;
+    use bedrs::{Bed12, Bed3, Bed6, Coordinates, Strand};
+
+    #[test]
+    fn test_flank_left_bounds() {
+        let iv = Bed3::new(1, 10, 400);
+        let left = left_flank(iv, 50).unwrap();
+        assert_eq!(left.start(), 1);
+        assert_eq!(left.end(), 10);
+    }
+
+    #[test]
+    fn test_flank_left_zero() {
+        let iv = Bed3::new(1, 10, 400);
+        let left = left_flank(iv, 0);
+        assert!(left.is_none());
+    }
+
+    #[test]
+    fn test_flank_left_bed3() {
+        let iv = Bed3::new(1, 100, 400);
+        let left = left_flank(iv, 50).unwrap();
+        assert_eq!(left.start(), 50);
+        assert_eq!(left.end(), 100);
+    }
+
+    #[test]
+    fn test_flank_left_bed6() {
+        let iv = Bed6::new(1, 100, 400, 1, 2, Strand::default());
+        let left = left_flank(iv, 50).unwrap();
+        assert_eq!(left.start(), 50);
+        assert_eq!(left.end(), 100);
+        assert_eq!(left.name(), iv.name());
+        assert_eq!(left.score(), iv.score());
+        assert_eq!(left.strand(), iv.strand());
+    }
+
+    #[test]
+    fn test_flank_left_bed12() {
+        let iv = Bed12::new(1, 100, 400, 1, 2, Strand::default(), 3, 4, 5, 6, 7, 8);
+        let left = left_flank(iv, 50).unwrap();
+        assert_eq!(left.start(), 50);
+        assert_eq!(left.end(), 100);
+        assert_eq!(left.name(), iv.name());
+        assert_eq!(left.score(), iv.score());
+        assert_eq!(left.strand(), iv.strand());
+        assert_eq!(left.thick_start(), iv.thick_start());
+        assert_eq!(left.thick_end(), iv.thick_end());
+        assert_eq!(left.item_rgb(), iv.item_rgb());
+        assert_eq!(left.block_count(), iv.block_count());
+        assert_eq!(left.block_sizes(), iv.block_sizes());
+        assert_eq!(left.block_starts(), iv.block_starts());
+    }
+
+    #[test]
+    fn test_flank_right_bounds() {
+        let genome = Genome::from_params(1, 100);
+        let iv = Bed3::new(1, 10, 80);
+        let right = right_flank(iv, 50, Some(&genome)).unwrap();
+        assert_eq!(right.start(), 80);
+        assert_eq!(right.end(), 100);
+    }
+
+    #[test]
+    fn test_flank_right_zero() {
+        let iv = Bed3::new(1, 10, 400);
+        let right = right_flank(iv, 0, None);
+        assert!(right.is_none());
+    }
+
+    #[test]
+    fn test_flank_right_bed3() {
+        let iv = Bed3::new(1, 100, 400);
+        let right = right_flank(iv, 50, None).unwrap();
+        assert_eq!(right.start(), 400);
+        assert_eq!(right.end(), 450);
+    }
+
+    #[test]
+    fn test_flank_right_bed6() {
+        let iv = Bed6::new(1, 100, 400, 1, 2, Strand::default());
+        let right = right_flank(iv, 50, None).unwrap();
+        assert_eq!(right.start(), 400);
+        assert_eq!(right.end(), 450);
+        assert_eq!(right.name(), iv.name());
+        assert_eq!(right.score(), iv.score());
+        assert_eq!(right.strand(), iv.strand());
+    }
+
+    #[test]
+    fn test_flank_right_bed12() {
+        let iv = Bed12::new(1, 100, 400, 1, 2, Strand::default(), 3, 4, 5, 6, 7, 8);
+        let right = right_flank(iv, 50, None).unwrap();
+        assert_eq!(right.start(), 400);
+        assert_eq!(right.end(), 450);
+        assert_eq!(right.name(), iv.name());
+        assert_eq!(right.score(), iv.score());
+        assert_eq!(right.strand(), iv.strand());
+        assert_eq!(right.thick_start(), iv.thick_start());
+        assert_eq!(right.thick_end(), iv.thick_end());
+        assert_eq!(right.item_rgb(), iv.item_rgb());
+        assert_eq!(right.block_count(), iv.block_count());
+        assert_eq!(right.block_sizes(), iv.block_sizes());
+        assert_eq!(right.block_starts(), iv.block_starts());
+    }
+
+    #[test]
+    fn test_flank_both() {
+        let iv = Bed3::new(1, 100, 400);
+        let mut it = flank_interval(iv, 50, 50, None);
+        let left = it.next().unwrap();
+        assert_eq!(left.start(), 50);
+        assert_eq!(left.end(), 100);
+        let right = it.next().unwrap();
+        assert_eq!(right.start(), 400);
+        assert_eq!(right.end(), 450);
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_flank_both_no_left() {
+        let iv = Bed3::new(1, 100, 400);
+        let mut it = flank_interval(iv, 0, 50, None);
+        let right = it.next().unwrap();
+        assert_eq!(right.start(), 400);
+        assert_eq!(right.end(), 450);
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_flank_both_no_right() {
+        let iv = Bed3::new(1, 100, 400);
+        let mut it = flank_interval(iv, 50, 0, None);
+        let left = it.next().unwrap();
+        assert_eq!(left.start(), 50);
+        assert_eq!(left.end(), 100);
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_flank_none() {
+        let iv = Bed3::new(1, 100, 400);
+        let mut it = flank_interval(iv, 0, 0, None);
+        assert!(it.next().is_none());
+    }
 }
