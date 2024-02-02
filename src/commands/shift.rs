f871a209bdac615ce216c6a438bbfe73ef0453d5
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-02-02 12:15:18 -0800

    tests: added testing functions for shift

--- src/commands/shift.rs
+++ src/commands/shift.rs
@@ -138,3 +138,110 @@ pub fn shift(
     }
     dispatch_shift(bed, genome_path, amount, percent, output_handle)
 }
+
+#[cfg(test)]
+mod testing {
+
+    use super::*;
+    use bedrs::{Bed12, Bed3, Bed6, Coordinates, Strand};
+
+    #[test]
+    fn test_calculate_percentage() {
+        let iv = Bed3::new(1, 100, 200);
+        assert_eq!(calculate_percentage(iv, 0.5), 50);
+        assert_eq!(calculate_percentage(iv, 0.1), 10);
+        assert_eq!(calculate_percentage(iv, 0.0), 0);
+        assert_eq!(calculate_percentage(iv, 1.0), 100);
+        assert_eq!(calculate_percentage(iv, 1.5), 150);
+    }
+
+    #[test]
+    fn test_shift_negative() {
+        let iv = Bed3::new(1, 100, 200);
+        let si = shift_interval(iv, -50.0, false, None);
+        assert_eq!(si.start(), 50);
+        assert_eq!(si.end(), 150);
+    }
+
+    #[test]
+    fn test_shift_left_bounds() {
+        let iv = Bed3::new(1, 100, 200);
+        let si = shift_interval(iv, -150.0, false, None);
+        assert_eq!(si.start(), 1);
+        assert_eq!(si.end(), 50);
+    }
+
+    #[test]
+    fn test_shift_right_bounds_end() {
+        let iv = Bed3::new(1, 100, 200);
+        let genome = Genome::from_params(1, 300);
+        let si = shift_interval(iv, 150.0, false, Some(&genome));
+        assert_eq!(si.start(), 250);
+        assert_eq!(si.end(), 300);
+    }
+
+    #[test]
+    fn test_shift_right_bounds_both() {
+        let iv = Bed3::new(1, 100, 200);
+        let genome = Genome::from_params(1, 300);
+        let si = shift_interval(iv, 250.0, false, Some(&genome));
+        assert_eq!(si.start(), 300);
+        assert_eq!(si.end(), 300);
+    }
+
+    #[test]
+    fn test_shift_fractional() {
+        let iv = Bed3::new(1, 100, 200);
+        let si = shift_interval(iv, 0.5, true, None);
+        assert_eq!(si.start(), 150);
+        assert_eq!(si.end(), 250);
+
+        let si = shift_interval(iv, 0.1, true, None);
+        assert_eq!(si.start(), 110);
+        assert_eq!(si.end(), 210);
+
+        let si = shift_interval(iv, -0.1, true, None);
+        assert_eq!(si.start(), 90);
+        assert_eq!(si.end(), 190);
+
+        let si = shift_interval(iv, 2.0, true, None);
+        assert_eq!(si.start(), 300);
+        assert_eq!(si.end(), 400);
+    }
+
+    #[test]
+    fn test_shift_bed3() {
+        let iv = Bed3::new(1, 100, 200);
+        let si = shift_interval(iv, 50.0, false, None);
+        assert_eq!(si.start(), 150);
+        assert_eq!(si.end(), 250);
+    }
+
+    #[test]
+    fn test_shift_bed6() {
+        let iv = Bed6::new(1, 100, 200, 1, 2, Strand::default());
+        let si = shift_interval(iv, 50.0, false, None);
+        assert_eq!(si.start(), 150);
+        assert_eq!(si.end(), 250);
+        assert_eq!(si.name(), iv.name());
+        assert_eq!(si.score(), iv.score());
+        assert_eq!(si.strand(), iv.strand());
+    }
+
+    #[test]
+    fn test_shift_bed12() {
+        let iv = Bed12::new(1, 100, 400, 1, 2, Strand::default(), 3, 4, 5, 6, 7, 8);
+        let si = shift_interval(iv, 50.0, false, None);
+        assert_eq!(si.start(), 150);
+        assert_eq!(si.end(), 450);
+        assert_eq!(si.name(), iv.name());
+        assert_eq!(si.score(), iv.score());
+        assert_eq!(si.strand(), iv.strand());
+        assert_eq!(si.thick_start(), iv.thick_start());
+        assert_eq!(si.thick_end(), iv.thick_end());
+        assert_eq!(si.item_rgb(), iv.item_rgb());
+        assert_eq!(si.block_count(), iv.block_count());
+        assert_eq!(si.block_sizes(), iv.block_sizes());
+        assert_eq!(si.block_starts(), iv.block_starts());
+    }
+}
