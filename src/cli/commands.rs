dea1377ae68b5c445cc464f3d659b2d0aac67d8d
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-02-07 08:27:39 -0800

    refactor: move cli to separate module

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -1,21 +1,5 @@
 use crate::types::{FieldFormat, InputFormat};
-use clap::{Parser, Subcommand};
-
-#[derive(Parser)]
-#[clap(version)]
-pub struct Cli {
-    /// Subcommand to run
-    #[clap(subcommand)]
-    pub command: Command,
-
-    /// Compression threads to use for output files if applicable
-    #[clap(global = true, short = 'j', long, default_value = "1")]
-    pub compression_threads: usize,
-
-    /// Compression level to use for output files if applicable
-    #[clap(global = true, long, default_value = "6")]
-    pub compression_level: u32,
-}
+use clap::Subcommand;
 
 #[derive(Subcommand)]
 pub enum Command {
---
+++ src/cli/mod.rs
@@ -0,0 +1,20 @@
+mod commands;
+pub use commands::Command;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version)]
+pub struct Cli {
+    /// Subcommand to run
+    #[clap(subcommand)]
+    pub command: Command,
+
+    /// Compression threads to use for output files if applicable
+    #[clap(global = true, short = 'j', long, default_value = "1")]
+    pub compression_threads: usize,
+
+    /// Compression level to use for output files if applicable
+    #[clap(global = true, long, default_value = "6")]
+    pub compression_level: u32,
+}
