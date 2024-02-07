8325c14ad49c5e92138bb2e7d871c0a9bb91bda6
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-02-07 09:29:55 -0800

    feat: added direct type conversion to inputs

--- src/cli/inputs.rs
+++ src/cli/inputs.rs
@@ -1,4 +1,8 @@
-use crate::types::{FieldFormat, InputFormat};
+use crate::{
+    io::BedReader,
+    types::{FieldFormat, InputFormat},
+};
+use anyhow::{bail, Result};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -15,6 +19,11 @@ pub struct SingleInput {
     #[clap(short = 'N', long)]
     pub field_format: Option<FieldFormat>,
 }
+impl SingleInput {
+    pub fn get_reader(self) -> Result<BedReader> {
+        BedReader::from_path(self.input, self.input_format, self.field_format)
+    }
+}
 
 #[derive(Parser, Debug)]
 pub struct DualInput {
@@ -25,4 +34,20 @@ pub struct DualInput {
     /// Secondary BED file to use
     #[clap(short, long)]
     pub b: String,
+}
+impl DualInput {
+    pub fn reader_from_a(&self) -> Result<BedReader> {
+        BedReader::from_path(self.a.clone(), None, None)
+    }
+    pub fn reader_from_b(&self) -> Result<BedReader> {
+        BedReader::from_path(Some(self.b), None, None)
+    }
+    pub fn get_readers(self) -> Result<(BedReader, BedReader)> {
+        let bed_a = self.reader_from_a()?;
+        let bed_b = self.reader_from_b()?;
+        if bed_a.is_named() != bed_b.is_named() {
+            bail!("Input files must both be either named or unnamed");
+        }
+        Ok((self.reader_from_a()?, self.reader_from_b()?))
+    }
 }
