d48c931723db82d69ee7c27e53dd0dedad223bea
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-02-28 08:20:15 -0800

    refactor: made implementation of Reorder generic to avoid code bloat

--- src/types/translate/reorder.rs
+++ src/types/translate/reorder.rs
@@ -10,61 +10,12 @@ where
         translater: Translater,
     ) -> Retranslater;
 }
-impl Reorder<NumericBed3> for NumericBed3 {
+impl<I> Reorder<I> for I
+where
+    I: IntervalBounds<usize, usize>,
+{
     fn reorder_translater(
-        set: &mut IntervalContainer<Self, usize, usize>,
-        translater: Translater,
-    ) -> Retranslater {
-        let retranslate = translater.lex_sort();
-        set.apply_mut(|iv| {
-            let new_chr = retranslate.get_rank(*iv.chr()).unwrap();
-            iv.update_chr(&new_chr);
-        });
-        retranslate
-    }
-}
-impl Reorder<NumericBed4> for NumericBed4 {
-    fn reorder_translater(
-        set: &mut IntervalContainer<Self, usize, usize>,
-        translater: Translater,
-    ) -> Retranslater {
-        let retranslate = translater.lex_sort();
-        set.apply_mut(|iv| {
-            let new_chr = retranslate.get_rank(*iv.chr()).unwrap();
-            iv.update_chr(&new_chr);
-        });
-        retranslate
-    }
-}
-impl Reorder<NumericBed6> for NumericBed6 {
-    fn reorder_translater(
-        set: &mut IntervalContainer<Self, usize, usize>,
-        translater: Translater,
-    ) -> Retranslater {
-        let retranslate = translater.lex_sort();
-        set.apply_mut(|iv| {
-            let new_chr = retranslate.get_rank(*iv.chr()).unwrap();
-            iv.update_chr(&new_chr);
-        });
-        retranslate
-    }
-}
-impl Reorder<NumericBed12> for NumericBed12 {
-    fn reorder_translater(
-        set: &mut IntervalContainer<Self, usize, usize>,
-        translater: Translater,
-    ) -> Retranslater {
-        let retranslate = translater.lex_sort();
-        set.apply_mut(|iv| {
-            let new_chr = retranslate.get_rank(*iv.chr()).unwrap();
-            iv.update_chr(&new_chr);
-        });
-        retranslate
-    }
-}
-impl Reorder<NumericMetaInterval> for NumericMetaInterval {
-    fn reorder_translater(
-        set: &mut IntervalContainer<Self, usize, usize>,
+        set: &mut IntervalContainer<I, usize, usize>,
         translater: Translater,
     ) -> Retranslater {
         let retranslate = translater.lex_sort();
