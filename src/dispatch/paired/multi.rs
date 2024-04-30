98930d8bbbf871dba33e3762351c44a5657d27b8
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-04-30 11:53:18 -0700

    refactor: use nested macro system for multi and standard input dispatch

--- src/dispatch/paired/multi.rs
+++ src/dispatch/paired/multi.rs
@@ -1,5 +1,3 @@
-/// This is a macro to match the LHS and RHS BED format and dispatch to some function with some
-/// parameters and a writer.
 #[macro_export]
 macro_rules! dispatch_pair_multi {
     ($reader_a:expr, $reader_b:expr, $writer:expr, $params:expr, $func:expr) => {{
@@ -12,147 +10,75 @@ macro_rules! dispatch_pair_multi {
     }};
 }
 
-/// This is a macro to match the LHS BED format and dispatch to the RHS
 #[macro_export]
 macro_rules! dispatch_to_lhs_multi_rhs {
-    ($reader_a: expr, $reader_b:expr, $translater: expr, $writer:expr, $params:expr, $func:expr) => {
+    ($reader_a:expr, $multi_reader:expr, $translater:expr, $writer:expr, $params:expr, $func:expr) => {
+        $crate::format_to_lhs_multi_rhs!(
+            $reader_a,
+            $multi_reader,
+            $translater,
+            $writer,
+            $params,
+            $func,
+            (Bed3, bed3_set_with),
+            (Bed4, bed4_set_with),
+            (Bed6, bed6_set_with),
+            (Bed12, bed12_set_with),
+            (Gtf, gtf_set_with),
+            (Ambiguous, meta_interval_set_with),
+            (BedGraph, bedgraph_set_with)
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! dispatch_to_multi_rhs {
+    ($set_a:expr, $multi_reader:expr, $translater:expr, $writer:expr, $params:expr, $func:expr) => {
+        $crate::format_to_multi_rhs!(
+            $set_a,
+            $multi_reader,
+            $translater,
+            $writer,
+            $params,
+            $func,
+            (Bed3, into_bed3_set_with),
+            (Bed4, into_bed4_set_with),
+            (Bed6, into_bed6_set_with),
+            (Bed12, into_bed12_set_with),
+            (Gtf, into_gtf_set_with),
+            (BedGraph, into_bedgraph_set_with),
+            (Ambiguous, into_meta_interval_set_with)
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! format_to_lhs_multi_rhs {
+    ($reader_a:expr, $multi_reader:expr, $translater:expr, $writer:expr, $params:expr, $func:expr, $( ($fmt:ident, $method:ident) ),*) => {
         match $reader_a.input_format() {
-            InputFormat::Bed3 => {
-                let set_a = $reader_a.bed3_set_with($translater.as_mut())?;
-                $crate::dispatch_to_multi_rhs!(
-                    set_a,
-                    $reader_b,
-                    $translater,
-                    $writer,
-                    $params,
-                    $func
-                )
-            }
-            InputFormat::Bed4 => {
-                let set_a = $reader_a.bed4_set_with($translater.as_mut())?;
-                $crate::dispatch_to_multi_rhs!(
-                    set_a,
-                    $reader_b,
-                    $translater,
-                    $writer,
-                    $params,
-                    $func
-                )
-            }
-            InputFormat::Bed6 => {
-                let set_a = $reader_a.bed6_set_with($translater.as_mut())?;
-                $crate::dispatch_to_multi_rhs!(
-                    set_a,
-                    $reader_b,
-                    $translater,
-                    $writer,
-                    $params,
-                    $func
-                )
-            }
-            InputFormat::Bed12 => {
-                let set_a = $reader_a.bed12_set_with($translater.as_mut())?;
-                $crate::dispatch_to_multi_rhs!(
-                    set_a,
-                    $reader_b,
-                    $translater,
-                    $writer,
-                    $params,
-                    $func
-                )
-            }
-            InputFormat::Gtf => {
-                let set_a = $reader_a.gtf_set_with($translater.as_mut())?;
-                $crate::dispatch_to_multi_rhs!(
-                    set_a,
-                    $reader_b,
-                    $translater,
-                    $writer,
-                    $params,
-                    $func
-                )
-            }
-            InputFormat::Ambiguous => {
-                let set_a = $reader_a.meta_interval_set_with($translater.as_mut())?;
-                $crate::dispatch_to_multi_rhs!(
-                    set_a,
-                    $reader_b,
-                    $translater,
-                    $writer,
-                    $params,
-                    $func
-                )
-            }
-            InputFormat::BedGraph => {
-                let set_a = $reader_a.bedgraph_set_with($translater.as_mut())?;
-                $crate::dispatch_to_multi_rhs!(
-                    set_a,
-                    $reader_b,
-                    $translater,
-                    $writer,
-                    $params,
-                    $func
-                )
-            }
+            $(
+                InputFormat::$fmt => {
+                    let set_a = $reader_a.$method($translater.as_mut())?;
+                    $crate::dispatch_to_multi_rhs!(set_a, $multi_reader, $translater, $writer, $params, $func)
+                },
+            )*
         }
     };
 }
 
-/// This is a macro to match the RHS BED format and dispatch to some function with some
-/// parameters and a writer.
 #[macro_export]
-macro_rules! dispatch_to_multi_rhs {
-    ($set_a: expr, $multi_reader:expr, $translater: expr, $writer:expr, $params:expr, $func:expr) => {{
+macro_rules! format_to_multi_rhs {
+    ($set_a:expr, $multi_reader:expr, $translater:expr, $writer:expr, $params:expr, $func:expr, $( ($fmt:ident, $method:ident) ),*) => {
         match $multi_reader[0].input_format() {
-            InputFormat::Bed3 => {
-                let mut set_b = IntervalContainer::empty();
-                for reader in $multi_reader {
-                    reader.into_bed3_set_with(&mut set_b, $translater.as_mut())?;
-                }
-                $func($set_a, set_b, $translater.as_ref(), $params, $writer)
-            }
-            InputFormat::Bed4 => {
-                let mut set_b = IntervalContainer::empty();
-                for reader in $multi_reader {
-                    reader.into_bed4_set_with(&mut set_b, $translater.as_mut())?;
-                }
-                $func($set_a, set_b, $translater.as_ref(), $params, $writer)
-            }
-            InputFormat::Bed6 => {
-                let mut set_b = IntervalContainer::empty();
-                for reader in $multi_reader {
-                    reader.into_bed6_set_with(&mut set_b, $translater.as_mut())?;
-                }
-                $func($set_a, set_b, $translater.as_ref(), $params, $writer)
-            }
-            InputFormat::Bed12 => {
-                let mut set_b = IntervalContainer::empty();
-                for reader in $multi_reader {
-                    reader.into_bed12_set_with(&mut set_b, $translater.as_mut())?;
-                }
-                $func($set_a, set_b, $translater.as_ref(), $params, $writer)
-            }
-            InputFormat::Gtf => {
-                let mut set_b = IntervalContainer::empty();
-                for reader in $multi_reader {
-                    reader.into_gtf_set_with(&mut set_b, $translater.as_mut())?;
-                }
-                $func($set_a, set_b, $translater.as_ref(), $params, $writer)
-            }
-            InputFormat::BedGraph => {
-                let mut set_b = IntervalContainer::empty();
-                for reader in $multi_reader {
-                    reader.into_bedgraph_set_with(&mut set_b, $translater.as_mut())?;
-                }
-                $func($set_a, set_b, $translater.as_ref(), $params, $writer)
-            }
-            InputFormat::Ambiguous => {
-                let mut set_b = IntervalContainer::empty();
-                for reader in $multi_reader {
-                    reader.into_meta_interval_set_with(&mut set_b, $translater.as_mut())?;
-                }
-                $func($set_a, set_b, $translater.as_ref(), $params, $writer)
-            }
+            $(
+                InputFormat::$fmt => {
+                    let mut set_b = IntervalContainer::empty();
+                    for reader in $multi_reader {
+                        reader.$method(&mut set_b, $translater.as_mut())?;
+                    }
+                    $func($set_a, set_b, $translater.as_ref(), $params, $writer)
+                },
+            )*
         }
-    }};
+    };
 }
